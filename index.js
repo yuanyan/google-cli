a589dfea9d87b1277e8f1ed0d89c7355e2adb643
youxiachai <youxiachai@gmail.com>
2014-07-07 14:16:40 +0800

    fix google query browser not open

--- index.js
+++ index.js
@@ -56,8 +56,9 @@ function search(q){
         }else{
             url = 'http://' + ip;
         }
-        open(url);
-        process.exit(0);
+        open(url, function () {
+            process.exit(0);
+        });
     })
 }
 
